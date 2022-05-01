608e271db73817e32cc65903ccd4b7ffb52819cb
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-05-01 18:00:38 +0300

    relate the owner and the apartment

--- property/models.py
+++ property/models.py
@@ -98,8 +98,16 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField(
+        'ФИО владельца',
+        max_length=200,
+        db_index=True
+    )
+    owners_phonenumber = models.CharField(
+        'Номер владельца',
+        max_length=20,
+        db_index=True
+    )
     owner_pure_phone = PhoneNumberField(
         blank=True,
         default=''
