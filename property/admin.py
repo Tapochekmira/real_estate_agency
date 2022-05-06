f4703b5230cdb461273e35eef24ecdbdb93c4c0d
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-05-06 21:04:13 +0300

    use decorator to register in admin

--- property/admin.py
+++ property/admin.py
@@ -7,6 +7,7 @@ class FlatsInline(admin.TabularInline):
     raw_id_fields = ("owner", )
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ('created_at',)
@@ -25,16 +26,15 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ('liked_by', )
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat')
     list_display = ('user', 'flat', 'complaint_text')
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats', )
     list_display = ('owner', 'owners_phonenumber', 'owner_pure_phone')
 
 
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
